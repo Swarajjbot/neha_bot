e76298f5b55904cde97b697ca22ee9dfaf78ab81
Swaraj16090 <156641910+Swaraj16090@users.noreply.github.com>
2024-03-26 09:45:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -219,11 +219,10 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """@INDORECINEMAS5
-{file_name}
+    CAPTION = """[ @INDORECINEMAS5 ] - {file_name}
 
 #IDRCINEMAS<b> #𝙉𝙤𝟏
- 🎭𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗  𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡⚡.
+ 🎭𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗  𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡.
 
 ❖𝗝𝐎𝐈𝐍 🎬 - @INDORECINEMAS5 🍿
 
